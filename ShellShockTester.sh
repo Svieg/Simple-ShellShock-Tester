56bb0ce5c95acf0a157f9aadf24b7b5ee9ff8f0a
Hugo Genesse <svieg.ashcasce@gmail.com>
2014-09-25 14:08:39 -0400

    1.2: Corrected echo date file output that bypasses the patch

--- ShellShockTester.sh
+++ ShellShockTester.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 env X='() { :;}; echo' /bin/cat /etc/passwd; echo 'Welcome to he Simple ShellShock Tester By Svieg';echo 'Your infos are at risk';
 env x='() { :;}; echo Your system is vulnerable update ASAP' bash -c "echo Visit svieg.wordpress.com for update info"
-X='() { (a)=>\' bash -c "echo date"
+env X='() { (a)=>\' bash -c "echo date"
