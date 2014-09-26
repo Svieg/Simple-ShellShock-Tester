562cbe8aaf4cb965bbd0c1593c4097edaef4e838
Hugo Genesse <svieg.ashcasce@gmail.com>
2014-09-26 15:16:23 -0400

    Corrected the tester

--- ShellShockTester.sh
+++ ShellShockTester.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-env X='() { :;}; echo' /bin/cat /etc/passwd; echo 'Welcome to he Simple ShellShock Tester By Svieg';echo 'Your infos are at risk';
+echo 'Welcome to he Simple ShellShock Tester By Svieg';echo 'Your infos may be at risk';
 env x='() { :;}; echo Your system is vulnerable update ASAP' bash -c "echo Visit svieg.wordpress.com for update info"
 env X='() { (a)=>\' bash -c "echo date"
