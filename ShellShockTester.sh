5cd0e385b1e98e531e9e7ac23b029854ee1390b1
Hugo Genesse <svieg.ashcasce@gmail.com>
2014-09-26 15:21:25 -0400

    Spelling error

--- ShellShockTester.sh
+++ ShellShockTester.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo 'Welcome to he Simple ShellShock Tester By Svieg';echo 'Your infos may be at risk';
+echo 'Welcome to the Simple ShellShock Tester By Svieg';echo 'Your infos may be at risk';
 env x='() { :;}; echo Your system is vulnerable update ASAP' bash -c "echo Visit svieg.wordpress.com for update info"
 env X='() { (a)=>\' bash -c "echo date"
